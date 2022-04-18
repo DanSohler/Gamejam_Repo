9b860df8dc9ef4052ec673c5e6081a8f11cb0009
Daniel Sohler <daniel.sohler73@gmail.com>
2022-04-18 17:58:55 +1000

    Setup Dan_Test

---
+++ Gamejam_Proj/Assets/Scripts/Player/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        //  Tracks a set target and hsa a lerp to smooth the following effect
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
