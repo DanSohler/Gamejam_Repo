85bb7972abef09aa55ef9bc5bc850351d651c77c
Daniel Sohler <daniel.sohler73@gmail.com>
2022-04-19 21:24:10 +1000

    Added academic gain function

--- Gamejam_Proj/Assets/Scripts/AcademicManager.cs
+++ Gamejam_Proj/Assets/Scripts/AcademicManager.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class AcademicManager : MonoBehaviour
 {
-    
+    public bool inAca = false;
+    public WellbeingManager wellbeingManager;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inAca = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inAca = false;
+        }
+    }
 }
