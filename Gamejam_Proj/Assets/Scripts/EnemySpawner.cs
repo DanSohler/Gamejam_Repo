f016382bf6185dc0f0e79e7f5a06c5f383530fde
Daniel Sohler <daniel.sohler73@gmail.com>
2022-04-19 21:03:54 +1000

    Enemy spawning is 'working'

---
+++ Gamejam_Proj/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public RegenVolume regenScript;
+
+    [SerializeField] private GameObject[] walkerType = new GameObject[4];
+    [SerializeField] private GameObject[] shooterType = new GameObject[4];
+
+    [SerializeField] private GameObject[] spawnLocationArray = new GameObject[4];
+
+    private int spawnLocation;
+    public float spawnDelay;
+    private float compChoice;
+
+    public void SpawnEnemy()
+    {
+        compChoice = Random.Range(0, 2);
+        if (compChoice == 0)
+        {
+            spawnLocation = Random.Range(0, 4);
+            StartCoroutine(spawnWalk(spawnDelay, walkerType[regenScript.currentState]));
+        }
+        else
+        {
+            spawnLocation = Random.Range(0, 4);
+            StartCoroutine(spawnShoot(spawnDelay, shooterType[regenScript.currentState]));
+        }
+    }
+
+    public void Despawn()
+    {
+        var clones = GameObject.FindGameObjectsWithTag ("Enemy");
+        foreach (var clone in clones)
+        {
+            Destroy(clone);
+        }
+    }
+
+
+
+
+    public IEnumerator spawnWalk(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, spawnLocationArray[spawnLocation].transform.position,Quaternion.identity);
+
+    }
+
+    public IEnumerator spawnShoot(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, spawnLocationArray[spawnLocation].transform.position, Quaternion.identity);
+
+    }
+
+}
--- Gamejam_Proj/Assets/Scripts/Fundamentals/RegenVolume.cs
+++ Gamejam_Proj/Assets/Scripts/Fundamentals/RegenVolume.cs
@@ -5,82 +5,133 @@ using UnityEngine;
 public class RegenVolume : MonoBehaviour
 {
     public WellbeingManager wellbeingManager;
-    [Tooltip("Social, Physical, Academic, Financial")]
-    public string currentState;
+    public EnemySpawner enemySpawner;
+    [Tooltip("0 = Social, 1 = Physical, 2 = Academic, 3 = Financial")]
+    public int currentState;
+    public bool playerEntered = false;
+    public float spawnDelay;
+    private bool coroutineBreaker = false;
 
-    
+
     #region Checks
+
+
+    void EnemyCall()
+    {
+        coroutineBreaker = false;
+        StartCoroutine(spawnEnemyInScene());
+    }
+
+    void EnemyCull()
+    {
+        enemySpawner.Despawn();
+        coroutineBreaker = true;
+        StopCoroutine(spawnEnemyInScene());
+        enemySpawner.Despawn();
+    }
+
 
     //Regen Volume
     private void OnTriggerEnter(Collider other)
     {
-        if (currentState == "Social")
+        if (currentState == 0)
         {
             if (other.gameObject.tag == "Player")
             {
                 wellbeingManager.socialRegen = true;
+                EnemyCall();
+                playerEntered = true;
             }
         }
 
-        if (currentState == "Physical")
+        if (currentState == 1)
         {
             if (other.gameObject.tag == "Player")
             {
                 wellbeingManager.physicalRegen = true;
+                EnemyCall();
+                playerEntered = true;
             }
         }
 
-        if (currentState == "Academic")
+        if (currentState == 2)
         {
             if (other.gameObject.tag == "Player")
             {
                 wellbeingManager.academicRegen = true;
+                EnemyCall();
+                playerEntered = true;
             }
         }
 
-        if (currentState == "Financial")
+        if (currentState == 3)
         {
             if (other.gameObject.tag == "Player")
             {
                 wellbeingManager.moneyRegen = true;
+                EnemyCall();
+                playerEntered = true;
             }
         }
     }
 
+    //cleanup section
     private void OnTriggerExit(Collider other)
     {
-        if (currentState == "Social")
+        if (currentState == 0)
         {
             if (other.gameObject.tag == "Player")
             {
+                EnemyCull();
                 wellbeingManager.socialRegen = false;
+                playerEntered = false;
+                EnemyCull();
             }
         }
 
-        if (currentState == "Physical")
+        if (currentState == 1)
         {
             if (other.gameObject.tag == "Player")
             {
+                EnemyCull();
                 wellbeingManager.physicalRegen = false;
+                playerEntered = false;
+                EnemyCull();
             }
         }
 
-        if (currentState == "Academic")
+        if (currentState == 2)
         {
             if (other.gameObject.tag == "Player")
             {
+                EnemyCull();
                 wellbeingManager.academicRegen = false;
+                playerEntered = false;
+                EnemyCull();
             }
         }
 
-        if (currentState == "Financial")
+        if (currentState == 3)
         {
             if (other.gameObject.tag == "Player")
             {
+                EnemyCull();
                 wellbeingManager.moneyRegen = false;
+                playerEntered = false;
+                EnemyCull();
             }
         }
     }
 
     #endregion
+
+
+    public IEnumerator spawnEnemyInScene()
+    {
+        while (playerEntered && !coroutineBreaker)
+        {
+            enemySpawner.SpawnEnemy();
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
 }
