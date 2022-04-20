3af4db1c78432b5c21583764baa9695036dce146
Daniel Sohler <daniel.sohler73@gmail.com>
2022-04-20 13:10:02 +1000

    minor AI tweaks

--- Gamejam_Proj/Assets/Scripts/Enemies/EnemyController.cs
+++ Gamejam_Proj/Assets/Scripts/Enemies/EnemyController.cs
@@ -1,27 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
     private Rigidbody myRB;
     public float moveSpeed;
 
+   // public Transform player;
+    public NavMeshAgent agent;
+
     public TDC_Script thePlayer;
 
-    void Start()
+    private void Awake()
     {
         myRB = GetComponent<Rigidbody>();
+
         thePlayer = FindObjectOfType<TDC_Script>();
+        agent = GetComponent<NavMeshAgent>();
+
+        //player = GameObject.Find("ExampleController").transform;
+    }
+
+    public void MoveToPlayer()
+    {
+        transform.LookAt(thePlayer.transform);
+        agent.SetDestination(thePlayer.transform.position);
     }
 
     void FixedUpdate()
     {
-        myRB.velocity = (transform.forward * moveSpeed);
-    }
-
-    void Update()
-    {
-        transform.LookAt(new Vector3(thePlayer.transform.position.x, transform.position.y, thePlayer.transform.position.z));
+        //transform.LookAt(new Vector3(thePlayer.transform.position.x, transform.position.y, thePlayer.transform.position.z));
+        MoveToPlayer();
+       // myRB.velocity = (transform.forward * moveSpeed);
     }
 }
